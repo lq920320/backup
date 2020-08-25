a6d8c3cb6383c07c1a836f967200209c86311f70
Hunter-liu <liuqian920320@163.com>
2020-08-25 08:02:22 +0800

    Create LengthOfLongestSubstring.java

---
+++ java/LengthOfLongestSubstring.java
@@ -0,0 +1,73 @@
+package leetcode;
+
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author liuqian
+ * @date 2020/8/14 17:33
+ */
+public class LengthOfLongestSubstring {
+
+    /**
+     * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+     *
+     * 示例 1:
+     *
+     * 输入: "abcabcbb"
+     * 输出: 3
+     * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+     *
+     * 示例 2:
+     *
+     * 输入: "bbbbb"
+     * 输出: 1
+     * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+     *
+     * 示例 3:
+     *
+     * 输入: "pwwkew"
+     * 输出: 3
+     * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     *      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+     *
+     */
+    @Test
+    public void lengthOfLongestSubstringTest() {
+        String s1 = "abcabcbb";
+        String s2 = "bbbbb";
+        String s3 = "pwwkew";
+
+        System.out.println(lengthOfLongestSubstring(s1));
+        System.out.println(lengthOfLongestSubstring(s2));
+        System.out.println(lengthOfLongestSubstring(s3));
+    }
+
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.equals("")) {
+            return 0;
+        }
+        // 哈希集合，记录每个字符是否出现过
+        Set<Character> occ = new HashSet<>();
+        int n = s.length();
+        // 右指针，初始值为 -1，相当于我们再字符串的左边界的左侧，还没有开始移动
+        int rk = -1;
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i != 0) {
+                // 左指针向右移动一格，移除一格字符
+                occ.remove(s.charAt(i - 1));
+            }
+            while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
+                // 不断地移动右指针
+                occ.add(s.charAt(rk + 1));
+                ++rk;
+            }
+            // 第 i 到 rk 个字符是一个极长的无重复字符子串
+            ans = Math.max(ans, rk - i + 1);
+        }
+        return ans;
+    }
+}
